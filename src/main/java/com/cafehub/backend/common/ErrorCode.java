fcab138d689cd66e3ae1b794ed1b4f8e54dd9034
RYUL99 <seunglul1999@gmail.com>
2024-08-06 14:28:29 +0900

    [INIT] ResponseDTO 추가

---
+++ src/main/java/com/cafehub/backend/common/ErrorCode.java
@@ -0,0 +1,4 @@
+package com.cafehub.backend.common;
+
+public enum ErrorCode {
+}
---
+++ src/main/java/com/cafehub/backend/common/ResponseDTO.java
@@ -0,0 +1,26 @@
+package com.cafehub.backend.common;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ResponseDTO <T>{
+
+    private Boolean success;
+
+    private  T data;
+
+    private ErrorCode errorCode;
+
+
+    public static <T> ResponseDTO<T>  success(T data){
+
+        return new ResponseDTO<>(true,data,null);
+    }
+
+    public static <T> ResponseDTO<T> fail(ErrorCode errorCode){
+        return new ResponseDTO<>(false,null,errorCode);
+    }
+}
