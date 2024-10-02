cece8a21d1ecc95652abc51445d6bdb86da1cfef
RYUL99 <seunglul1999@gmail.com>
2024-10-03 02:14:03 +0900

    [Init] 임시 CORS 설정 파일 추가

---
+++ src/main/java/com/cafehub/backend/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.cafehub.backend.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 임시 Cors 설정 파일 추가, 추후 삭제 예정
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // 노트북 React 실행 시 , 프론트 배포된 서버에서 요청날아올 시 ,
+                .allowedOrigins("http://localhost:3000", "https://main.d3fr2u7nicdqc9.amplifyapp.com")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
+    }
+}
