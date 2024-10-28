ed379de710ae06f89ca3caaea2e781b015bdc26e
RYUL99 <seunglul1999@gmail.com>
2024-10-28 16:50:35 +0900

    ✨[Feat] 리뷰 댓글 전체보기 기능 추가

--- src/main/java/com/cafehub/backend/domain/comment/controller/CommentController.java
+++ src/main/java/com/cafehub/backend/domain/comment/controller/CommentController.java
@@ -2,7 +2,9 @@ package com.cafehub.backend.domain.comment.controller;
 
 
 import com.cafehub.backend.common.dto.ResponseDTO;
+import com.cafehub.backend.domain.comment.dto.request.AllCommentGetRequestDTO;
 import com.cafehub.backend.domain.comment.dto.request.CommentCreateRequestDTO;
+import com.cafehub.backend.domain.comment.dto.response.AllCommentGetResponseDTO;
 import com.cafehub.backend.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentService commentService;
+
+
+    @GetMapping("/optional-auth/reviews/{reviewId}/comments/{currentPage}")
+    public ResponseEntity<ResponseDTO<AllCommentGetResponseDTO>> getAllComment(@PathVariable Long reviewId,
+                                                                               @PathVariable Integer currentPage){
+
+        AllCommentGetRequestDTO requestDTO = AllCommentGetRequestDTO.builder()
+                .reviewId(reviewId)
+                .currentPage(currentPage)
+                .build();
+
+        return ResponseEntity.ok(commentService.getAllCommentsBySlice(requestDTO));
+    }
+
+
 
 
     @PostMapping("/auth/reviews/{reviewId}/comment")
--- src/main/java/com/cafehub/backend/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/cafehub/backend/domain/comment/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.cafehub.backend.domain.comment.repository;
 import com.cafehub.backend.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> , CommentRepositoryCustom{
+    List<Comment> findALlByMemberId(Long memberId);
 }
